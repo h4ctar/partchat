c8fd0de961c408d1a99538b93c1f08eb59f94179
Ben Smith <ben.smith@startmail.com>
2023-03-22 10:53:30 +1100

    home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,12 @@
 export const Home = () => {
     return (
         <div className="p-5">
-            <h1>Home</h1>
+            <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+                <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+                    Highly organised community developed technical information
+                    to help maintain bikes and cars.
+                </h1>
+            </div>
         </div>
     );
 };
