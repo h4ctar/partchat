22b60745df8626c4873ed36765d2e034c7b48846
Ben Smith <ben.smith@startmail.com>
2023-03-23 20:30:28 +1100

    cleaner motorcycle page

--- src/motorcycles/Motorcycle.tsx
+++ src/motorcycles/Motorcycle.tsx
@@ -1,13 +1,15 @@
-import { useMotorcycle } from "./motorcycle.hook";
+import { Link } from "wouter";
+import { useDiagrams, useMotorcycle } from "./motorcycle.hook";
 
 type Params = {
     motorcycleId: string;
 };
 
 export const Motorcycle = ({ motorcycleId }: Params) => {
-    const { query } = useMotorcycle(motorcycleId);
+    const { query: queryMotorcycle } = useMotorcycle(motorcycleId);
+    const { query } = useDiagrams(motorcycleId);
 
-    if (!query.data) {
+    if (!queryMotorcycle.data || !query.data) {
         return (
             <div className="p-5">
                 <h1>Loading...</h1>
@@ -16,9 +18,52 @@ export const Motorcycle = ({ motorcycleId }: Params) => {
     }
 
     return (
-        <div className="p-5">
-            <h1 className="font-bold text-2xl mb-4">{`${query.data.make} ${query.data.model} ${query.data.yearFrom}-${query.data.yearTo}`}</h1>
-            <img src={query.data.image} />
-        </div>
+        <>
+            <div className="p-5 flex flex-col items-center">
+                <h1 className="font-bold text-2xl mb-4">{`${queryMotorcycle.data.make} ${queryMotorcycle.data.model} ${queryMotorcycle.data.yearFrom}-${queryMotorcycle.data.yearTo}`}</h1>
+                <div className="flex flex-row gap-5 items-center">
+                    <img
+                        className="rounded-lg"
+                        src={queryMotorcycle.data.image}
+                    />
+                    <table>
+                        <thead>
+                            <tr>
+                                <th className="my-th" rowSpan={2}>
+                                    Specifications
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="bg-white dark:bg-slate-800">
+                            <tr>
+                                <td className="my-td">Capacity</td>
+                                <td className="my-td">653 cc</td>
+                            </tr>
+                            <tr>
+                                <td className="my-td">Bore x Stroke</td>
+                                <td className="my-td">63 x 52.4 mm</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="p-5 grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
+                {query.data.map((diagram) => (
+                    <Link
+                        key={diagram.id}
+                        href={`/motorcycles/${motorcycleId}/diagrams/${diagram.id}`}
+                        className="rounded-lg dark:bg-slate-800 flex flex-row"
+                    >
+                        <img
+                            className="rounded-l-lg h-96"
+                            src={diagram.image}
+                        />
+                        <h2 className="font-medium text-xlg m-3">
+                            {diagram.name}
+                        </h2>
+                    </Link>
+                ))}
+            </div>
+        </>
     );
 };
