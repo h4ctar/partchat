bd42b49614041471b452efac462a43700380c049
Ben Smith <ben.smith@startmail.com>
2023-03-27 19:39:22 +1100

    change style

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,14 +3,14 @@ import { LoginButton } from "./auth/LoginButton";
 export const Home = () => {
     return (
         <div className="p-5">
-            <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-                <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+            <div className="relative max-w-7xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+                <h1 className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white text-center">
                     Organised library of technical information to help maintain
                     old bikes and cars.
                 </h1>
             </div>
-            <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-                <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+            <div className="relative max-w-7xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+                <h1 className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white text-center">
                     Reddit style discussions pinned to vehicles, diagrams and
                     parts.
                 </h1>
@@ -18,8 +18,8 @@ export const Home = () => {
                     Easily find the thread you need.
                 </p>
             </div>
-            <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-                <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+            <div className="relative max-w-7xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+                <h1 className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white text-center">
                     Compatible part catalogue.
                 </h1>
                 <p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
@@ -27,8 +27,8 @@ export const Home = () => {
                     provide them.
                 </p>
             </div>
-            <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-                <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+            <div className="relative max-w-7xl mx-auto p-20 sm:pt-24 lg:pt-32">
+                <h1 className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white text-center">
                     Offline mode.
                 </h1>
                 <p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
