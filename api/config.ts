a43055c155beec1113ba115033e7b6bc82174408
Ben Smith <ben.smith@startmail.com>
2023-03-29 15:32:07 +1100

    connect get /config to prisma

--- api/config.ts
+++ api/config.ts
@@ -1,50 +1,73 @@
+import { Motorcycle } from "@prisma/client";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import _ from "lodash";
-import { MOTORCYCLES } from "../prisma/_data";
+import { prisma } from "./_prisma";
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
     if (request.method === "GET") {
         console.info("Get config");
-        const config = getMakes().reduce(
+
+        const motorcycles = await prisma.motorcycle.findMany();
+
+        // This gross bit of code builds the config for the make -> year -> model selectors
+        // I'm sure it can be done a lot better
+        // {
+        //     Honda: {
+        //         1975: {
+        //             CB400F: "honda-cb400f-1975-1977",
+        //         },
+        //     },
+        // };
+        const config = getMakes(motorcycles).reduce(
             (makes, make) => ({
                 ...makes,
-                [make]: getYears(make).reduce(
+                [make]: getYears(motorcycles, make).reduce(
                     (years, year) => ({
                         ...years,
-                        [year]: getMotorcycles(make, year).reduce(
+                        [year]: getMotorcycles(motorcycles, make, year).reduce(
                             (models, motorcycle) => ({
                                 ...models,
                                 [motorcycle.model]: motorcycle.id,
                             }),
-                            {}
+                            {},
                         ),
                     }),
-                    {}
+                    {},
                 ),
             }),
-            {}
+            {},
         );
+
         response.status(200).send(config);
     } else {
         throw new Error("Unsupported method");
     }
 };
 
-const getMakes = () => _.uniq(MOTORCYCLES.map((motorcycle) => motorcycle.make));
-const getYears = (make: string) =>
+const getMakes = (motorcycles: Motorcycle[]) =>
+    _.uniq(motorcycles.map((motorcycle) => motorcycle.make));
+
+const getYears = (motorcycles: Motorcycle[], make: string) =>
     _.uniq(
-        MOTORCYCLES.filter((motorcycle) => motorcycle.make === make).flatMap(
-            (motorcycle) =>
+        motorcycles
+            .filter((motorcycle) => motorcycle.make === make)
+            .flatMap((motorcycle) =>
                 Array(motorcycle.yearTo - motorcycle.yearFrom + 1)
                     .fill(0)
-                    .map((_, index) => index + motorcycle.yearFrom)
-        )
+                    .map((_, index) => index + motorcycle.yearFrom),
+            ),
     );
-const getMotorcycles = (make: string, year: number) =>
+
+const getMotorcycles = (
+    motorcycles: Motorcycle[],
+    make: string,
+    year: number,
+) =>
     _.uniq(
-        MOTORCYCLES.filter((motorcycle) => motorcycle.make === make)
+        motorcycles
+            .filter((motorcycle) => motorcycle.make === make)
             .filter((motorcycle) => motorcycle.yearFrom <= year)
-            .filter((motorcycle) => motorcycle.yearTo >= year)
+            .filter((motorcycle) => motorcycle.yearTo >= year),
     );
 
 export default handler;
