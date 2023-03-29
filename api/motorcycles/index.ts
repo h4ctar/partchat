14adf1bb845f6549c6b64e7103104a798b665001
Ben Smith <ben.smith@startmail.com>
2023-03-29 14:49:41 +1100

    prisma orderBy instead of sort

--- api/motorcycles/index.ts
+++ api/motorcycles/index.ts
@@ -6,14 +6,17 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
     if (request.method === "GET") {
         console.info("Get all motorcycles");
 
-        const make = request.query.make as string;
-        const year = parseInt(request.query.year as string);
+        const make = (request.query.make as string) || undefined;
+        const year = parseInt(request.query.year as string) || undefined;
 
         const motorcycleModels = await prisma.motorcycle.findMany({
             where: {
                 make,
                 yearFrom: { lte: year },
                 yearTo: { gte: year },
+            },
+            orderBy: {
+                yearFrom: "asc",
             },
         });
 
@@ -26,8 +29,9 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
                         href: `/api/diagrams?motorcycleId=${motorcycleModel.id}`,
                     },
                 },
-            })
+            }),
         );
+
         response.status(200).send(motorcycleResources);
     } else {
         throw new Error("Unsupported method");
