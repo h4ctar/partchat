503ec73c5bd41abefe78c3aff570f0dd706c6eca
Ben Smith <ben.smith@startmail.com>
2023-03-29 14:13:39 +1100

    connect get /motorcycles/:id to prisma

--- api/motorcycles/[motorcycleId].ts
+++ api/motorcycles/[motorcycleId].ts
@@ -1,28 +1,34 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import _ from "lodash";
-import { MOTORCYCLES } from "../../prisma/_data";
+import { MotorcycleResource } from "../../types/motorcycles";
+import { prisma } from "../_prisma";
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
-    const motorcycleId = request.query.motorcycleId;
-    const motorcycle = MOTORCYCLES.find(
-        (motorcycle) => motorcycle.id === motorcycleId
-    );
-    if (!motorcycle) {
-        response.status(404).send("Motorcycle not found");
-        return;
-    }
+    const motorcycleId = request.query.motorcycleId as string;
 
     if (request.method === "GET") {
         console.info("Get motorcycle");
-        response.status(200).send({
-            ...motorcycle,
-            _links: {
-                self: { href: `/api/motorcycles/${motorcycle.id}` },
-                diagrams: {
-                    href: `/api/diagrams?motorcycleId=${motorcycle.id}`,
-                },
+
+        const motorcycleModel = await prisma.motorcycle.findUnique({
+            where: {
+                id: motorcycleId,
             },
         });
+
+        if (!motorcycleModel) {
+            response.status(404).send("Motorcycle not found");
+            return;
+        }
+
+        const motorcycleResource: MotorcycleResource = {
+            ...motorcycleModel,
+            _links: {
+                self: { href: `/api/motorcycles/${motorcycleId}` },
+                diagrams: {
+                    href: `/api/diagrams?motorcycleId=${motorcycleId}`,
+                },
+            },
+        };
+        response.status(200).send(motorcycleResource);
     } else {
         throw new Error("Unsupported method");
     }
