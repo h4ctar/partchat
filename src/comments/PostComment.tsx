0244d1dadaf4dae6320ecac386b008496a83b2e1
Ben Smith <ben.smith@startmail.com>
2023-04-02 06:48:40 +1000

    clear comment on success - resolves #21

--- src/comments/PostComment.tsx
+++ src/comments/PostComment.tsx
@@ -9,44 +9,58 @@ type Props = {
 };
 
 export const PostComment = ({ motorcycleId, diagramId, partId }: Props) => {
-    const [newCommentText, setNewCommentText] = useState("");
+    const [text, setText] = useState("");
     const { isAuthenticated, getAccessTokenSilently } = useAuth0();
     const [errorMessage, setErrorMessage] = useState("");
+    const [statusMessage, setStatusMessage] = useState("");
+    const [disabled, setDisabled] = useState(false);
 
     const handlePostComment = () => {
         setErrorMessage("");
+        setStatusMessage("Posting comment...");
+        setDisabled(true);
+
         postComment(
             { motorcycleId, diagramId, partId },
             {
-                text: newCommentText,
+                text,
                 motorcycleId,
                 diagramId,
                 partId,
             },
             getAccessTokenSilently,
-        ).catch(() => setErrorMessage("Failed to post new comment"));
+        )
+            .then(() => setText(""))
+            .catch(() => setErrorMessage("Failed to post new comment"))
+            .finally(() => {
+                setStatusMessage("");
+                setDisabled(false);
+            });
     };
 
     return (
         <div className="flex flex-col gap-4">
             <textarea
-                value={newCommentText}
-                onChange={(event) => setNewCommentText(event.target.value)}
+                value={text}
+                onChange={(event) => setText(event.target.value)}
                 className="w-full rounded-lg dark:bg-slate-800"
                 rows={4}
                 placeholder={
                     isAuthenticated ? "Write comment" : "Log in to comment"
                 }
-                disabled={!isAuthenticated}
+                disabled={disabled || !isAuthenticated}
             />
             <div className="flex flex-row-reverse justify-between items-center">
                 <button
                     onClick={handlePostComment}
-                    disabled={!isAuthenticated}
-                    className="font-semibold px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 dark:text-white dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
+                    disabled={disabled || !isAuthenticated}
+                    className="font-semibold px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 dark:text-white dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 disabled:dark:bg-slate-500 disabled:dark:text-slate-700"
                 >
                     {isAuthenticated ? "Post comment" : "Log in to comment"}
                 </button>
+                {statusMessage && (
+                    <div className="text-slate-300">{statusMessage}</div>
+                )}
                 {errorMessage && (
                     <div className="text-red-500">{errorMessage}</div>
                 )}
