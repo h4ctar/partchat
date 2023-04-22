2db08172c60f19c58b2e6e810e95696049f5e4e3
Ben Smith <ben.smith@startmail.com>
2023-04-23 08:48:49 +1000

    comment body style

--- src/comments/Comment.tsx
+++ src/comments/Comment.tsx
@@ -11,7 +11,7 @@ type Props = {
 export const Comment = ({ comment }: Props) => {
     return (
         <div className="dark:highlight-white/10 relative z-10 flex flex-col gap-6 rounded-lg bg-white p-5 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 md:flex-row">
-            <div className="flex flex-col gap-4 md:order-2">
+            <div className="flex flex-grow flex-col gap-4 md:order-2">
                 <div className="flex flex-row items-center gap-4">
                     <Avatar name={comment.username} />
                     <div className="text-lg font-bold">{comment.username}</div>
