e37e9241439083003b5c5be7e14f2e55b5df1b09
Ben Smith <ben.smith@startmail.com>
2023-04-24 08:25:05 +1000

    fix dark theme on first visit - resolves #54

--- src/settings.ts
+++ src/settings.ts
@@ -21,11 +21,7 @@ export const useSettings = () => {
     };
 
     const updateThemeClass = () => {
-        if (
-            localStorage.theme === "dark" ||
-            (!("theme" in localStorage) &&
-                window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
+        if (localStorage.theme === "dark") {
             document.documentElement.classList.add("dark");
         } else {
             document.documentElement.classList.remove("dark");
