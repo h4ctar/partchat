9c2a75bcda96db44d6aba4b6dc32c7a654feda83
Ben Smith <ben.smith@startmail.com>
2023-05-16 06:43:45 +1000

    font size in tables

--- src/ui/Table.tsx
+++ src/ui/Table.tsx
@@ -17,14 +17,14 @@ export const Table = ({
 }: Props) => {
     return (
         <div className="max-h-full overflow-auto rounded-lg shadow-xl ring-1 ring-slate-900/5">
-            <table className="border-separate border-spacing-0">
+            <table className="w-full border-separate border-spacing-0">
                 <tbody>
                     {headings && (
                         <tr className="group">
                             {headings?.map((heading, colIndex) => (
                                 <th
                                     key={colIndex}
-                                    className="border-b border-slate-100 p-4 text-left font-medium text-slate-400 group-first:first:rounded-tl-lg group-first:last:rounded-tr-lg group-last:first:rounded-bl-lg group-last:last:rounded-br-lg dark:border-slate-600 dark:text-slate-200"
+                                    className="border-b border-slate-100 p-4 text-sm font-medium text-slate-400 group-first:first:rounded-tl-lg group-first:last:rounded-tr-lg group-last:first:rounded-bl-lg group-last:last:rounded-br-lg dark:border-slate-600 dark:text-slate-200 lg:text-base"
                                 >
                                     {heading}
                                 </th>
@@ -46,7 +46,7 @@ export const Table = ({
                             {row.map((data, colIndex) => (
                                 <td
                                     key={colIndex}
-                                    className="border-b border-slate-100 p-4 group-first:first:rounded-tl-lg group-first:last:rounded-tr-lg group-last:first:rounded-bl-lg group-last:last:rounded-br-lg dark:border-slate-700"
+                                    className="border-b border-slate-100 p-4 text-sm group-first:first:rounded-tl-lg group-first:last:rounded-tr-lg group-last:first:rounded-bl-lg group-last:last:rounded-br-lg dark:border-slate-700 lg:text-base"
                                 >
                                     {data}
                                 </td>
