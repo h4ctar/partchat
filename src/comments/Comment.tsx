42d91668cce8280ad96f4db66e9558bd0f1e96fa
Ben Smith <ben.smith@startmail.com>
2023-05-22 07:13:09 +1000

    edit comment buttons

--- src/comments/Comment.tsx
+++ src/comments/Comment.tsx
@@ -3,6 +3,8 @@ import { CommentResource } from "../../types/motorcycles";
 import { DownArrowIcon } from "../icons/DownArrowIcon";
 import { UpArrowIcon } from "../icons/UpArrowIcon";
 import { RichHtml } from "../ui/rich-editor/RichHtml";
+import { PencilIcon } from "../icons/PencilIcon";
+import { TrashIcon } from "../icons/TrashIcon";
 
 type Props = {
     comment: CommentResource;
@@ -13,13 +15,27 @@ export const Comment = ({ comment }: Props) => {
         <div className="dark:highlight-white/10 relative z-10 flex flex-row flex-wrap gap-4 rounded-lg bg-white p-5 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800">
             <div className="flex flex-grow flex-row items-center gap-4">
                 <Avatar name={comment.username} />
-                <div className="text-lg font-bold">{comment.username}</div>
-                <div>{new Date(comment.createdAt).toLocaleDateString()}</div>
+                <div>
+                    <div className="text-lg font-bold">{comment.username}</div>
+                    <div className="text-xs">
+                        {new Date(comment.createdAt).toLocaleDateString()}
+                    </div>
+                </div>
             </div>
             <div className="flex flex-row items-center gap-4">
-                <UpArrowIcon />
+                <button>
+                    <PencilIcon />
+                </button>
+                <button>
+                    <TrashIcon />
+                </button>
+                <button>
+                    <UpArrowIcon />
+                </button>
                 <div className="text-lg font-bold">0</div>
-                <DownArrowIcon />
+                <button>
+                    <DownArrowIcon />
+                </button>
             </div>
             <div className="w-full">
                 <div className="prose prose-sm dark:prose-invert md:prose-base">
