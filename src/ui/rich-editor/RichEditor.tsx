3b7a41ea59ab4f37240f501c6104cb0c7c8421ce
Ben Smith <ben.smith@startmail.com>
2023-05-21 20:30:49 +1000

    better comment editor focus

--- src/ui/rich-editor/RichEditor.tsx
+++ src/ui/rich-editor/RichEditor.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { createEditor, Descendant } from "slate";
-import { Editable, Slate, withReact } from "slate-react";
+import { Editable, Slate, withReact, ReactEditor } from "slate-react";
 import { BoldIcon } from "../../icons/BoldIcon";
 import { CodeIcon } from "../../icons/CodeIcon";
 import { HeadingIcon } from "../../icons/HeadingIcon";
@@ -36,7 +36,7 @@ export const RichEditor = ({
     return (
         <Slate editor={editor} value={initialValue} onChange={onChange}>
             <div className="w-full rounded-lg border border-gray-300 dark:border-slate-50/10 dark:bg-slate-400/10 dark:bg-slate-800">
-                <div className="flex flex-row gap-4 border-b border-gray-300 p-4 dark:border-slate-50/10 dark:bg-slate-400/10 dark:bg-slate-800">
+                <div className="flex flex-row gap-4 border-b border-gray-300 p-4 dark:border-slate-50/10">
                     <LeafStyleButton style="bold">
                         <BoldIcon />
                     </LeafStyleButton>
@@ -53,7 +53,10 @@ export const RichEditor = ({
                         <QuoteIcon />
                     </BlockStyleButton>
                 </div>
-                <div className="p-5">
+                <div
+                    className="cursor-text p-5"
+                    onClick={() => ReactEditor.focus(editor)}
+                >
                     <Editable
                         renderElement={Element}
                         renderLeaf={Leaf}
