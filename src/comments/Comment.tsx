1b6022f5afe848e49cabff6c99e6cc8ffe1f3b73
Ben Smith <ben.smith@startmail.com>
2023-05-23 18:31:51 +1000

    remove queries for deleted comment

--- src/comments/Comment.tsx
+++ src/comments/Comment.tsx
@@ -23,6 +23,7 @@ export const Comment = ({ commentId }: Props) => {
     const mutation = useMutation({
         mutationFn: () => deleteComment(commentId, getAccessTokenSilently),
         onSuccess: async () => {
+            queryClient.removeQueries(["comments", commentId]);
             queryClient.invalidateQueries({
                 queryKey: ["comments"],
             });
