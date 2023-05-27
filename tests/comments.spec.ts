1e63e449b14e1d3ffb24240fd01bda9738d186d6
Ben Smith <ben.smith@startmail.com>
2023-05-28 07:07:20 +1000

    e2e tests

--- tests/comments.spec.ts
+++ tests/comments.spec.ts
@@ -5,8 +5,10 @@ test.describe("Comments", () => {
         await page.goto("/");
         await page.getByText("Log in").click();
 
-        await page.getByLabel("Username or email address").fill("zarniwoop");
-        await page.getByLabel("Password").fill("nW5U3hT4fDsrjD");
+        await page
+            .getByLabel("Username or email address")
+            .fill(process.env.TEST_USERNAME!);
+        await page.getByLabel("Password").fill(process.env.TEST_PASSWORD!);
         await page
             .getByRole("button", { name: "Continue", exact: true })
             .click();
