2b07b669ab4ebdaffdc8d0458b19d6be4b7ced8e
Ben Smith <ben.smith@startmail.com>
2023-06-29 21:11:46 +1000

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: "partchat-backend",
+            cwd: "backend",
+            script: "./dist/server.js",
+        },
+    ],
+
+    deploy: {
+        production: {
+            user: process.env.DEPLOY_USER,
+            host: process.env.DEPLOY_HOST,
+            key: process.env.DEPLOY_KEY,
+            ref: "origin/main",
+            repo: "https://github.com/h4ctar/partchat.git",
+            path: "/opt/partchat",
+            "post-deploy": "npm run post-deploy",
+        },
+    },
+};
