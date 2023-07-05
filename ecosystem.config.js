65827673e78de6d563c336ed63f736bb38e8a83f
Ben Smith <ben.smith@startmail.com>
2023-07-06 09:10:10 +1000

    ssh test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,6 @@ module.exports = {
         production: {
             user: process.env.SSH_USER,
             host: process.env.DEPLOY_HOST,
-            ssh_options: "StrictHostKeyChecking=no",
             key: "~/.ssh/gh_rsa",
             ref: "origin/main",
             repo: "https://github.com/h4ctar/partchat.git",
