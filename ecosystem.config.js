072c201a7837fd3aa1fec03ec13f34e3586f209e
Ben Smith <ben.smith@startmail.com>
2023-07-06 11:31:31 +1000

    add deploy env variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
         production: {
             user: process.env.SSH_USER,
             host: process.env.DEPLOY_HOST,
-            key: "~/.ssh/gh_rsa",
+            key: "~/.ssh/github_rsa",
             ref: "origin/main",
             repo: "https://github.com/h4ctar/partchat.git",
             path: "/opt/partchat",
