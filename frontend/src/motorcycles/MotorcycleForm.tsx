173df6d07d28dba9ef1966765c0c3e2b24b256ee
Ben Smith <ben.smith@startmail.com>
2023-07-29 08:39:12 +1000

    change create motorcycle to add motorcycle

--- frontend/src/motorcycles/MotorcycleForm.tsx
+++ frontend/src/motorcycles/MotorcycleForm.tsx
@@ -148,7 +148,7 @@ export const MotorcycleForm = ({ motorcycleId }: Props) => {
                     className="dark:highlight-white/20 rounded-lg bg-slate-900 px-6 py-2 text-center font-semibold text-white hover:bg-slate-700 focus:outline-none dark:bg-sky-500 dark:hover:bg-sky-400"
                     type="submit"
                     value={
-                        motorcycleId ? "Update motorcycle" : "Create motorcycle"
+                        motorcycleId ? "Update motorcycle" : "Add motorcycle"
                     }
                 />
             </form>
