999b07bc8aa3437a7d7752fcf778f55fddd17a29
Ben Smith <ben.smith@startmail.com>
2023-07-29 08:39:27 +1000

    update motorcycle e2e tests

--- test/specs/motorcycles.spec.ts
+++ test/specs/motorcycles.spec.ts
@@ -2,11 +2,46 @@ import { expect, test } from "@playwright/test";
 import { login } from "./util";
 
 test.describe("Motorcycles", () => {
-    test("Create, edit and delete motorcycles", async ({ page }) => {
+    test("Add, edit and delete motorcycles", async ({ page }) => {
+        const motorcycleMake = `Make${Math.round(Math.random() * 100)}`;
+        const motorcycleModel = `Model${Math.round(Math.random() * 100)}`;
+        const motorcycleId = `${motorcycleMake.toLowerCase()}-${motorcycleModel.toLowerCase()}`;
+
         await login(page);
         await page.goto("/motorcycles");
 
+        // Add motorcycle
         await page.getByRole("link", { name: "Add motorcycle" }).click();
+        await page.waitForURL("/motorcycles/new");
+
+        await page.getByLabel("Make").fill(motorcycleMake);
+        await page.getByLabel("Model").fill(motorcycleModel);
+        await page.getByLabel("Year from").fill("1900");
+        await page.getByLabel("Year to").fill("2000");
+        await page.getByLabel("Engine Type").fill("4 Cylinder");
+        await page.getByLabel("Displacementccm").fill("1000");
+        await page.getByLabel("Valves per cylinder").fill("4");
+        await page.getByLabel("PowerkW").fill("100");
+        await page.getByLabel("Compression:1").fill("10");
+        await page.getByLabel("Top Speedkm/h").fill("200");
+        await page.getByLabel("Weightkg").fill("200");
+
+        await page.getByRole("button", { name: "Add motorcycle" }).click();
+        await page.waitForURL(`/motorcycles/${motorcycleId}`);
+
+        await expect(
+            page.getByRole("heading", {
+                name: `${motorcycleMake} ${motorcycleModel}`,
+            }),
+        ).toBeVisible();
+
+        // Edit motorcycle
+
+        // Delete motorcycle
+        page.on("dialog", (dialog) => dialog.accept());
+        await page.locator('button[name="delete"]').click();
+
+        await page.waitForURL("/motorcycles");
 
         // Clean up
         await page.close();
@@ -16,14 +51,12 @@ test.describe("Motorcycles", () => {
         test("Create, edit and delete motorcycle comments", async ({
             page,
         }) => {
-            const commentText = `Test comment ${Math.round(
-                Math.random() * 100,
-            )}`;
+            const commentText = `Comment ${Math.round(Math.random() * 100)}`;
 
             await login(page);
             await page.goto("/motorcycles/yamaha-xj650lj-1982-1984");
 
-            // Create comment
+            // Post comment
             await page.getByRole("textbox").fill(commentText);
             await page.getByText("Post comment").click();
 
