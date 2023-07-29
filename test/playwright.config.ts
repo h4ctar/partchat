ae7330c0bf6865ab57866a64e47e8a06ece00a27
Ben Smith <ben.smith@startmail.com>
2023-07-30 07:57:28 +1000

    configure playwright test reporter

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     workers: 1,
 
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: process.env.CI ? "dot" : "list",
+    reporter: process.env.CI ? "html" : "list",
 
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
