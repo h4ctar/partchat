f6077417b603e7bbe1a726d22ff4766a268e7697
Ben Smith <ben.smith@startmail.com>
2026-04-25 10:01:04 +1000

    build: hardcode database url

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const DATABASE_URL = "file:./partchat.db";
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const adapter = new PrismaBetterSqlite3({ url: DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
